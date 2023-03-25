512c68fd71126137098dad976b8d38a25097b52d
Arut Aerian <arut.aerian@icloud.com>
2023-03-25 19:19:45 +0100

    Homework43.003

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("I am Superman");
+
+    Superman kent = new Superman();
+    kent.fly();
+    kent.run();
+    kent.swim();
+
   }
 }
---
+++ src/Superman.java
@@ -0,0 +1,20 @@
+public class Superman implements CanFly,CanRun,CanSwim
+{
+
+  @Override
+  public void fly() {
+    System.out.println("i can Fly");
+  }
+  public void run() {
+    System.out.println("i can Run");
+
+  }
+
+  public void swim() {
+    System.out.println("i can Swim");
+  }
+
+
+
+
+}
